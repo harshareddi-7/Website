63332c79162970b76cb55883e189e0756857afcc
harshareddi-7 <harshareddy95099@gmail.com>
2024-08-05 12:42:05 +0530

    fs

---
+++ assignment/data.js
@@ -0,0 +1,9 @@
+let employees=[{id:101,name:"Anju",Salary:45000},
+    {id:102,name:"Rahul",Salary:50000},
+    {id:103,name:"Harsha",Salary:60000}
+]
+// wap js code snippet to display the employee data in the form of html table
+ function data(){
+    document.getElementsByTagName('tbody')[0].innerHTML=employees.map
+
+    }
