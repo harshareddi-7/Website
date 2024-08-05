9d74e163a3f4a4e3b3e7f29ae94ff7f1759ad226
harshareddi-7 <harshareddy95099@gmail.com>
2024-08-05 13:19:37 +0530

    js

--- assignment/data.js
+++ assignment/data.js
@@ -4,6 +4,34 @@ let employees=[{id:101,name:"Anju",Salary:45000},
 ]
 // wap js code snippet to display the employee data in the form of html table
  function data(){
-    document.getElementsByTagName('tbody')[0].innerHTML=employees.map
+    /* let rows="";
+
+    for (emp of employees){
+        rows+=`<tr>
+        <td>${emp.id}</td>
+        <td>${emp.name}</td>
+        <td>${emp.Salary}</td></tr>`
+    } */
+
+    // using for loop
+    let rows="";
+    let i=0;
+    for (i=0; i<=employees.length-1; i++){
+        rows+=`<tr>
+        <td>${employees[i].id}</td>
+        <td>${employees[i].name}</td>
+        <td>${employees[i].Salary}</td></tr>`
+    } 
+// using while loop
+/* let rows="";
+let i=0;
+while(i<employees.length){
+    rows+=`<tr>
+        <td>${employees[i].id}</td>
+        <td>${employees[i].name}</td>
+        <td>${employees[i].Salary}</td></tr>`
+        i++
+} */
+    document.getElementsByTagName('tbody')[0].innerHTML=rows
 
     }
