4a8bdda4bda981ef0a1ed72ec1dbed8044bd8e22
harshareddi-7 <harshareddy95099@gmail.com>
2024-08-06 13:14:26 +0530

    functions ,events

---
+++ assignment/ass-2/Digital.js
@@ -0,0 +1,10 @@
+// document.getElementById('abc').innerText=new Date();
+/* document.getElementById('xyz').innerText=new Date().toLocaleTimeString();
+document.getElementsByTagName('p')[0].innerHTML=new Date().toTimeString();
+document.getElementsByTagName('p')[1].innerHTML=new Date().toLocaleDateString(); */
+// 
+
+setInterval(function(){
+    document.getElementsByTagName('p')[0].innerHTML=new Date().toTimeString();
+    
+},1000);
---
+++ in-Built.js/function.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    console.log(new Date());
+},1000);
+
+
+
+setInterval(function(){
+    console.log(new Date());
+},1000);
