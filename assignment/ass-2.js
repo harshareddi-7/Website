4ad8da3df68a11c3c8bb6990f0de3de84d8aace9
harshareddi-7 <harshareddy95099@gmail.com>
2024-08-07 13:33:51 +0530

    rs

---
+++ assignment/ass-2.js
@@ -0,0 +1,29 @@
+// var wish=name=>"hello"+name;
+// console.log(wish("Anju"));
+
+// function wish(name){
+//     return "hello" + name
+// }
+// let name=prompt("enter your name");
+// console.log(wish(name));
+
+
+// var wish=()=>{
+//     console.log("ga");
+// }
+// wish()
+
+
+// var wish=()=>{
+//     console.log("gm");
+// }
+// wish()
+
+function wish(){
+    console.log("ga");
+}
+wish()
+function wish(){
+    console.log("gm");
+}
+wish()
